f101414df74664cc5cbc783e00502736d10020f2
jibanCat <r04244007@ntu.edu.tw>
2020-11-16 00:10:26 +0800

    add a checking statement

--- slurm/submit_gadget.sh
+++ slurm/submit_gadget.sh
@@ -2,12 +2,17 @@
 
 # loop over folders
 for f in test*
-do
-    echo "submitting gadget files: $f ..."
-    
+do  
     # move into the folder and submit
     cd $f
-    sbatch mpi_submit
+
+    last_powerspec=`ls output | grep power | tail -n 1`
+    if [ $last_powerspec == 'powerspectrum-1.0000.txt' ]
+    then
+        echo "checking last powerspec file: $f ...$last_powerspec"
+        echo " ... submitting gadget files: $f"
+        sbatch mpi_submit
+    fi
 
     # move back to base folder
     cd ..
