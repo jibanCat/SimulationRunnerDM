56cae2adedcf038d1c80a9b919d4e317ac21042e
jibanCat <r04244007@ntu.edu.tw>
2020-11-15 23:57:42 +0800

    add a checking statement

--- slurm/submit_genics.sh
+++ slurm/submit_genics.sh
@@ -7,8 +7,12 @@ do
 
     # move into the folder and submit
     cd $f
-    sbatch mpi_submit_genic
-
+    num_files=`ls ICS | wc -l`
+    if [$num_files -eq 0]
+    then
+        echo "submitting genic IC files: $f"
+        sbatch mpi_submit_genic
+    fi
     # move back to base folder
     cd ..
 done
