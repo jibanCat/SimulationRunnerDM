521743e654eb61c002f97ccb4d21d974aff1b066
jibancat <mho026@ucr.edu>
2020-11-15 08:00:52 -0800

    slightly fix

--- slurm/submit_genics.sh
+++ slurm/submit_genics.sh
@@ -3,15 +3,13 @@
 # loop over folders
 for f in test*
 do
-    echo "submitting genic files: $f ..."
-
     # move into the folder and submit
     cd $f
     num_files=`ls ICS | wc -l`
-    if [$num_files -eq 0]
+    if [ $num_files -eq 0 ]
     then
         echo "submitting genic IC files: $f"
-        sbatch mpi_submit_genic
+        #sbatch mpi_submit_genic
     fi
     # move back to base folder
     cd ..
