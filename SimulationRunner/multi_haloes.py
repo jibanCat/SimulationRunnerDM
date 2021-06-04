19efea7b14112fca05880a81e0827617e0e62fe0
jibancat <mho026@ucr.edu>
2021-06-03 19:40:14 -0700

    init commit

---
+++ SimulationRunner/multi_haloes.py
@@ -0,0 +1,11 @@
+"""
+Compute halo mass functions and save into a HDF5 file
+"""
+from .multi_sims import GadgetLoad
+from .hmffromfof import HMFFromFOF
+
+class HaloMassFunction(GadgetLoad):
+
+    def __init__(self):
+        self
+    
