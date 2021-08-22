0cdeb594156b3140e187842b5126636c3f2408dd
jibancat <mho026@ucr.edu>
2021-08-22 15:36:48 -0700

    finish nbody powerspec

--- SimulationRunner/multi_nbodykit.py
+++ SimulationRunner/multi_nbodykit.py
@@ -2,7 +2,10 @@
 Loading derived summary statistics from nbodykit
 from multiple simulations.
 """
-from typing import Tuple
+from SimulationRunner.multi_sims import PowerSpec
+from typing import Tuple, List
+
+import os
 
 import numpy as np
 
@@ -62,9 +65,133 @@ def load_nbodykit_power(path: str, scale_factor: int, k_max = None,
     
     return k0, ps
 
-class NbodyKitPowerSpec:
+class NbodyKitPowerSpec(PowerSpec):
 
+    """
+    Loading power spectra from Nbodykit.
+
+    Loading all snaphots for MP-Gadget outputs.
+    Loading one snapshot for SR output.
+
+    Note: the training set will be directly outputed from MultiNbodyPowerSpec
+    """
+
+    def __init__(self, 
+        submission_dir: str = "test/", srgan: bool = False, z0 : float = 0.0, Ng: int = 512,
+        srgan_path: str = "super-resl/output/PART_008/powerspec_shotnoise.txt.npy") -> None:
+        super(NbodyKitPowerSpec, self).__init__(submission_dir)
+
+        # read into arrays
+        # Matter power specs from simulations
+        k0, ps = self.read_powerspec(z0=z0, Ng=Ng)
+
+        self._scale_factors = 1 / (1 + z0)
+
+        self._k0 = k0
+        self._powerspecs = ps
+
+        # Matter power specs from CAMB linear theory code
+        redshifts, out = self.read_camblinear(self.camb_files)
+
+        self._camb_redshifts = redshifts
+        self._camb_matters = out
+
+        # Matter power specs from SRGAN (conditioned on one redshift)
+        if srgan:
+            self.srgan_path = srgan_path
+            self._k0_sr, self._ps_sr = self.read_srgan_powerspec(srgan_path)
+
+    @property
+    def powerspecs(self) -> np.ndarray:
+        """
+        P(k) from PART/ folder. The same length as k0.
+        """
+        return self._powerspecs
+
+    @property
+    def k0(self) -> np.ndarray:
+        """
+        k from PART/ folder. The same length as powerspecs
+        """
+        return self._k0
+
+    @property
+    def powerspecs_srgan(self) -> np.ndarray:
+        """
+        P(k) from SRGAN.
+        """
+        return self._ps_sr
+    
+    @property
+    def k0_sr(self) -> np.ndarray:
+        """
+        k for SRGAN power spectrum.
+        """
+        return self._k0_sr
+
+    def read_powerspec(self, z0: float, Ng: int) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Read power spectrum from a PART/ folder
+
+        Parameters:
+        ----
+        z0 : the redshift of the power spectrum you want to load.
+        Ng : the number of particle per side you want to compute for the power spectrum.
+        """
+        tol = 1e-4 # tolerance
+        # the scale factor you condition on
+        scale_factor = 1 / (1 + z0)
+
+        # | No. of snapshot | scale factor |
+        # self._snapshots
+        if not np.any((self._snapshots[:, 1] - scale_factor) < tol):
+            print("Pick a scale factor in the list:", self._snapshots[:, 1])
+            assert np.any((self._snapshots[:, 1] - scale_factor) < tol)
+
+        # -1: if there are multiple matches, pick the final one (just personal preference)
+        ii = np.where((self._snapshots[:, 1] - scale_factor) < tol)[0][-1]
+
+        # PART_{number}
+        print("Found a={} is PART_{}.".format(scale_factor, self._snapshots[:, 0][ii]))
+        number = int(self._snapshots[:, 0][ii])
+
+        powerspec_path = os.path.join(
+            self.submission_dir, "output", "PART_{:03d}".format(number)
+        )
+
+        # the maximum k is controlled by Ng
+        k0, ps = load_nbodykit_power(
+            powerspec_path, scale_factor=scale_factor, k_max=None, subtract_shotnoise=False, times_kcubic=False, compensated=True, Ng=Ng)
+
+        return k0, ps
+
+
+    def read_srgan_powerspec(self, srgan_path: str) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Read the SRGAN power spectrum directly from the file.
+
+        SRGAN is conditioned on a single redshift, so be aware which redshift you applied before.
+        """
+        # load the SR
+        # exception happens when I wrongly saved the power spec as a binary file
+        try:
+            k0_sr, ps_sr = np.loadtxt(srgan_path)
+        except UnicodeDecodeError as e:
+            k0_sr, ps_sr = np.load(srgan_path, allow_pickle=True)
+        
+        return k0_sr, ps_sr
+
+
+class MultiNbodyKitPowerSpec:
+    """
+    Output a HDF5 file.
+
+    An additional function to output the training power spectra directly,
+    includes:
+        1. interpolate the LowRes (if necessary).
+        2. substract surrogate mean.
+        3. condition on z = 0.
+
+    """
     def __init__(self) -> None:
         pass
-
-    
